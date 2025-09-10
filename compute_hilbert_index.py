ed1198257c0a9745a0a76a09e0d1c3b82f5b8e79
chahine nicolas zede <chahine.nicolas.zede@gmail.com>
2025-09-10 21:34:42 +0200

    add arguments

--- compute_hilbert_index.py
+++ compute_hilbert_index.py
@@ -1,47 +1,61 @@
 import os
-import matplotlib.pyplot as plt 
-from module_loader_kitti_pose import * # add for more metrics
-from hilbertcurve.hilbertcurve import HilbertCurve
+import argparse
 import json
-##################################
-# read pos
-##################################
-eval_dataset = 'Kitti'
-eval_seq = 6
-kitti_dir = os.getenv('WORKSF') + "/datas/datasets/"
-eval_seq = '%02d' % eval_seq
-sequence_path = kitti_dir + 'sequences/' + eval_seq + '/'
-tfs, pose = load_poses_from_txt(sequence_path + 'poses.txt')
+import numpy as np
+import matplotlib.pyplot as plt
+from module_loader_kitti_pose import load_poses_from_txt
+from hilbertcurve.hilbertcurve import HilbertCurve
 
-min_bbox = np.min(pose,0) 
-pose = pose - min_bbox
+def main(eval_seq: int, p: int, save: bool, data_path: str):
+    ##################################
+    # Setup paths
+    ##################################
+    eval_seq_str = f"{eval_seq:02d}"
+    sequence_path = os.path.join(data_path, "sequences", eval_seq_str)
+
+    ##################################
+    # Load poses
+    ##################################
+    tfs, pose = load_poses_from_txt(os.path.join(sequence_path, "poses.txt"))
+
+    # Normalize pose so that min is (0,0,...)
+    min_bbox = np.min(pose, axis=0)
+    pose = pose - min_bbox
+
+    ##################################
+    # Hilbert curve mapping
+    ##################################
+    n = 2  # dimension fixed to 2
+    hilbert_curve = HilbertCurve(p, n)
+
+    dict_hilbert = {}
+    for i, coords in enumerate(pose):
+        xx = round(coords[0] * 100)
+        yy = round(coords[1] * 100)
+
+        res_str = str(hilbert_curve.distances_from_points([[xx, yy]])[0])
+        print(i, res_str)
+        dict_hilbert[res_str] = f"{i:06d}"
+
+    ##################################
+    # Save JSON (if requested)
+    ##################################
+
+    if save:
+        json_path = os.path.join(sequence_path, f"hilbert_{p}.json")
+        with open(json_path, "w") as json_file:
+            json.dump(dict_hilbert, json_file)
+        print("docid_map saved at", json_path)
 
 
-p = 16 
-n = 2   
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Hilbert curve mapping for KITTI poses")
+    kitti_dir = os.getenv("WORKSF") + "/datas/datasets/"
+    parser.add_argument("--data_path", type=str,  default=kitti_dir, required=True, help="Dataset path")
+    parser.add_argument("--eval_seq", type=int, required=True, help="Sequence number to evaluate (e.g., 6)")
+    parser.add_argument("--p", type=int, default=16, help="Hilbert curve iterations (default: 16)")
+    parser.add_argument("--save", type=bool, default=False, help="Save result as JSON")
 
-hilbert_curve = HilbertCurve(p, n)
-
-dict_hibert = {}
-for i in range(len(pose)):
-
-    label_id_gps = pose[int(i)]
-    xx = round(label_id_gps[0]*100)
-    yy = round(label_id_gps[1]*100)
-    
-    res_str = str(hilbert_curve.distances_from_points([[xx, yy]])[0])
-
-    print(i, str( res_str )  )
-    dict_hibert[ str( res_str ) ] = '%06d' % i
-
-
-
-json_path = sequence_path + 'hilbert_16.json'
-
-save = False
-if save:
-    with open(json_path, "w") as json_file:
-        json.dump(dict_hibert, json_file)  
-    print("docid_map saved at ", json_path)
-
+    args = parser.parse_args()
+    main(args.eval_seq, args.p, args.save, args.data_path) 
 
